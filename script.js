5ebc8bd53d7e4578b59261881c52b3b5e4328a66
gabrielymiller <gabrielycmiller@gmail.com>
2026-01-07 12:25:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let guestList = ["Karol", "Aidan"]; 
+let name = prompt("What is your first name? (first letter capitalized)"); 
+if (name === guestList[1]) { 
+    alert("You are my husband!!!! love you <3"); 
+} 
+else { 
+    alert("I don't know you, sorry"); 
+}
